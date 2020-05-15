9e6e7e154115c4dec9a37c001cc7fa6dbd965494
Keenan <keenanhallas@gmail.com>
2020-05-15 18:43:02 -0500

    form state written, name and size inputs added

--- ReadMe.md
+++ ReadMe.md
@@ -45,9 +45,9 @@ Follow these steps to set up and work on your project:
 
 Your MVP should include, at a minimum, the following components.
 
-- [ ] A homepage that has a "/" route and links to your form (button, nav bar, or any other type of link is acceptable)
-- [ ] A form with a "/pizza" route
-- [ ] A name text input field
+- [x] A homepage that has a "/" route and links to your form (button, nav bar, or any other type of link is acceptable)
+- [x] A form with a "/pizza" route
+- [x] A name text input field
 - [ ] Validation for name - name must be at least 2 characters
 - [ ] Dropdown form component for pizza size
 - [ ] Checklist form component for toppings - at least 4 (hint: name each separately!)
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,8 +1,59 @@
 import React, { useState } from "react";
 
 const PizzaForm = () => {
+    const [formState, setFormState] = useState({
+        name: "",
+        size: "",
+        sauce: "",
+        pepperoni: false,
+        sausage: false,
+        mushroom: false,
+        pineapple: false,
+        instructions: ""
+    });
+
+    const handleChange = () => {
+        console.log("Change logged!");
+    }
+
+    const handleSubmit = () => {
+        console.log("Submission logged!");
+    }
+
     return (
-        <h1>Hello world!</h1>
+        <form onSubmit={handleSubmit}>
+            <h2>Please enter your info</h2>
+            <label htmlFor="name">
+                Name:
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formState.name}
+                    onChange={handleChange}
+                />
+                {/*{errorState.name ? <ErrorMessage>{errorState.name}</ErrorMessage> : null}*/}
+            </label>
+            <br />
+            <label htmlFor="size">
+                Pizza size:
+                <select
+                    id="size"
+                    name="size"
+                    value={formState.name}
+                    onChange={handleChange}
+                >
+                    <option value="">--Please select a size--</option>
+                    <option value="S">Small</option>
+                    <option value="M">Medium</option>
+                    <option value="L">Large</option>
+                    <option value="XL">Extra Large</option>
+                </select>
+                {/*{errorState.name ? <ErrorMessage>{errorState.name}</ErrorMessage> : null}*/}
+            </label>
+            <br />
+            <button>Order now!</button>
+        </form>
     );
 };
 
