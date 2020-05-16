22add8391ac0b29b506b1e8b061a1f3c7af2795e
Keenan <keenanhallas@gmail.com>
2020-05-15 20:13:26 -0500

    first test complete

---
+++ cypress/integration/PizzaTests.js
@@ -0,0 +1,13 @@
+http://localhost:3000/pizza
+
+describe("Testing the pizza ordering form", () => {
+    it("Types a name into the name input", () => {
+        cy.visit("http://localhost:3000/pizza")
+        cy.get('#name')
+            .type("K")
+        cy.contains("Your name must be at least 2 characters.") 
+        cy.get('#name')  
+            .type("eenan Hallas")
+            .should("have.value", "Keenan Hallas")
+    })
+})
